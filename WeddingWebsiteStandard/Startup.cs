e4639c62914276ed4cf6829a3bfec3debab24ab7
Dukeboxz <u04sjj@gmail.com>
2019-08-13 20:23:01 +0100

    Add project files.

---
+++ WeddingWebsiteStandard/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WeddingWebsiteStandard.Startup))]
+namespace WeddingWebsiteStandard
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
