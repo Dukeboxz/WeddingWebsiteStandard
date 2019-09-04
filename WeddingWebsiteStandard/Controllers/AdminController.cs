bda24f1b8f65a23116d8bab17652426f64575c0f
Stephen Jackson <Stephen.Jackson@paulsmith.co.uk>
2019-09-04 17:03:03 +0100

    Created AdminPageViewModel class

--- WeddingWebsiteStandard/Controllers/AdminController.cs
+++ WeddingWebsiteStandard/Controllers/AdminController.cs
@@ -97,5 +97,7 @@ namespace WeddingWebsiteStandard.Controllers
             return View(guestVM); 
 
         }
+
+
     }
 }
---
+++ WeddingWebsiteStandard/Controllers/ViewModels/AdminPageViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WeddingWebsiteStandard.Models;
+
+namespace WeddingWebsiteStandard.Controllers.ViewModels
+{
+    public class AdminPageViewModel
+    {
+
+
+        public IEnumerable<Guest>  ListOfGuests {get; set;}
+
+        public IEnumerable<InvitationModel> LisOfInvitations { get; set; }
+
+        public IEnumerable<GiftModel> ListOfGifts { get; set; }
+
+
+    }
+}
